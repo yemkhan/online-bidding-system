e86ea32a9fe72e493e3d22ed53e8b9a47e338483
yem <2mail2mkhan@gmail.com>
2022-10-21 19:22:01 +0530

    fix: confirm changed

--- admin/index.php
+++ admin/index.php
@@ -82,7 +82,7 @@
     <div class="modal-dialog modal-md" role="document">
       <div class="modal-content">
         <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
+        <h5 class="modal-title">Confirmation Something </h5>
       </div>
       <div class="modal-body">
         <div id="delete_content"></div>
